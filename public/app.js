0be2e2a17fa7de815194ff045ff07c400c923dca
Kerubo0 <mmyers@kabarak.com>
2024-03-28 16:29:14 +0300

    edited navbar and font family

--- public/app.js
+++ public/app.js
@@ -1,11 +1,14 @@
-function showSidebar() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex';
-}
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
 
-function hideSidebar() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none';
+toggleBtn.onclick = function () {
+dropDownMenu.classList.toggle('open')
+const isOpen = dropDownMenu.classList.contains('open')
+
+toggleBtnIcon.classList  = isOpen 
+? 'fa-solid fa-xmark'
+: 'fa-solid fa-bars'
 }
 
 const backToTopBtn = document.getElementById("backToTopBtn");
